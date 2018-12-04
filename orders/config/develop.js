f7b14dd5e99e9205e82be6115eee4798494a934a
Bogdan7585 <Bogdan7585@yandex.ru>
2018-12-04 19:04:39 +0300

    1

--- orders/config/develop.js
+++ orders/config/develop.js
@@ -3,11 +3,17 @@
 exports.config = {
 	env: 'development',
 	name: 'users',
+	serviceRegistry: {
+		host: '192.168.0.110',
+		port: '8500'
+	},
 	listen: {
 		host: '127.0.0.1',
 		port: '8002'
 	},
 	db: {
-		path: './data/users.nedb'
+		type: 'mongodb',
+		url: 'mongodb://172.17.0.2:27017/orders',
+		path: './data/dbOrders.nedb'
 	}
 };
